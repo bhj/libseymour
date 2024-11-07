1f7f754f691f0491c2d30d801fa8ff75d8c1b4bf
Brandon Jones <brandon@radroot.com>
2024-11-06 23:48:26 -0500

    chore: initial project setup

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import globals from "globals"
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      globals: {
+        ...globals.browser,
+      },
+    },
+  }
+)
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+import path from 'path'
+
+export default {
+  mode: 'development',
+  entry: path.join(import.meta.dirname, 'src', 'main.ts'),
+  output: {
+	path: path.join(import.meta.dirname, 'src'),
+	filename: 'bundle.js'
+  },
+  plugins: [
+  ]
+}
