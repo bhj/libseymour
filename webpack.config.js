3305d04041abfb63b5ca1750e5b89281172fd976
Brandon Jones <brandon@radroot.com>
2024-11-10 22:04:14 -0500

    chore: add ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,20 @@ import path from 'path'
 export default {
   mode: 'development',
   entry: path.join(import.meta.dirname, 'src', 'main.ts'),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   output: {
     path: path.join(import.meta.dirname, 'src'),
     filename: 'bundle.js',
   },
-  plugins: [
-  ],
 }
